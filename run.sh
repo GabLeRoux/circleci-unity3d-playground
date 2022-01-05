ef7f699d5b2767ad5489ef860e9e7f499a8b65a6
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2022-01-05 08:45:34 -0500

    Search for UnityYAMLMerge

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# ls /opt/Unity/Editor/Data/PlaybackEngines/AndroidPlayer/SDK/tools/bin/sdkmanager
+# /opt/Unity/Editor/Data/PlaybackEngines/AndroidPlayer/SDK/tools/bin/sdkmanager --list
+# /opt/Unity/Editor/Data/PlaybackEngines/AndroidPlayer/SDK/tools/bin/sdkmanager --version
+
+# find UnityYAMLMerge
+find / -name UnityYAMLMerge
